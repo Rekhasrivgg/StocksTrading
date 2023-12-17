cc5336d6ed9b13688b3dd74ebb8e4a9316f30a6d
Rekhasrivgg <152485503+Rekhasrivgg@users.noreply.github.com>
2023-12-17 12:45:57 +0530

    UserExceptionHandler.java

--- src/main/java/com/app/stock/globalexceptionhandlers/UserExceptionHandler.java
+++ src/main/java/com/app/stock/globalexceptionhandlers/UserExceptionHandler.java
@@ -9,19 +9,34 @@ import com.app.stock.exception.CustomException;
 import com.app.stock.exception.PasswordMissmatchException;
 import com.app.stock.exception.UserNotFound;
 
+//Global Exception Handler for User
 @ControllerAdvice
 public class UserExceptionHandler {
 
+	/*
+	 * Handle CustomException.
+	 * Return ResponseEntity with a message and BAD_REQUEST status.
+	 */
 	@ExceptionHandler(value = CustomException.class)
 	public ResponseEntity<Object> exception() {
 		return new ResponseEntity<>("Person Already Exists!!!!", HttpStatus.BAD_REQUEST);
 
 	}
 
+	/*
+	 * Handle UserNotFound exception.
+	 *Return ResponseEntity with a message and NOT_FOUND status.
+	 */
+
 	@ExceptionHandler(value = UserNotFound.class)
 	public ResponseEntity<Object> userNotFoundException() {
 		return new ResponseEntity<>("Person Not Found!!!!", HttpStatus.NOT_FOUND);
 	}
+
+	/*
+	 * Handle PasswordMissmatchException.
+	 * Return ResponseEntity with a message and NOT_FOUND status.
+	 */
 
 	@ExceptionHandler(value = PasswordMissmatchException.class)
 	public ResponseEntity<Object> passwordNotMatch() {
