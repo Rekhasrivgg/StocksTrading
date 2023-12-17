eb6e3925fbb0756996980e3c21318ceac30405f0
Rekhasrivgg <152485503+Rekhasrivgg@users.noreply.github.com>
2023-12-17 12:39:24 +0530

    UserRepository.java

--- src/main/java/com/app/stock/repositories/UserRepository.java
+++ src/main/java/com/app/stock/repositories/UserRepository.java
@@ -6,12 +6,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.app.stock.entities.User;
-
+//Repository interface for User entity, extending JpaRepository.
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	/*
+	 * Retrieve a user by their email address.
+	 * Return Optional containing the user if found, empty otherwise.
+	 */
+    
 	Optional<User> findByUserEmail(String email);
-
+	
+	/*
+	 * Check if a user with the given email address exists.
+	 * Return True if a user with the given email address exists, false otherwise.
+	 */
 	boolean existsByUserEmail(String email);
 
 }
